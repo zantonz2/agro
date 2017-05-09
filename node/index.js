fbdf9f479c4b08e2132ca7c2892dfa952e2ab7e8
Anton Zhalybin <armagadon@mail.ru>
2017-05-09 18:28:39 +0600

    change2

--- node/index.js
+++ node/index.js
@@ -4,8 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.set('views', __dirname);
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index');
 });
 
 app.listen(app.get('port'), function () {
