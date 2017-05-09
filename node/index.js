1c325e7c68fc3e7e61206358c657008a35063bfc
Anton Zhalybin <armagadon@mail.ru>
2017-05-09 18:14:01 +0600

    change

--- node/index.js
+++ node/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Server listening on port', app.get('port'));
 });
